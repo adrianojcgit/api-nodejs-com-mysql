2550b8427a0af2967b16f397992ee1ad6ae6fa56
Adriano Cordeiro <adrianojcordeiro@gmail.com>
2025-04-29 11:14:05 -0300

    Ajuste para  instânciar o objeto do BD.

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -9,16 +9,34 @@ const router = express.Router();
 //Criar a rota cadastrar usuário
 router.post("/users", async (req: Request, res: Response) => {
     try{
+        //Receber os dados enviados no corpo da requisição
         var data = req.body;
-        console.log(data);
+        //Criar uma instância do repositório user
+        const userRepository = AppDataSource.getRepository(User);
+        //Recuperar o registro do banco de dados com o valor da coluna email
+        const existingUser = await userRepository.findOne({ where: {email: data.email}});
+        //Verficar se já existe usuário cadastrado com esse e-mail
+        if(existingUser){
+            res.status(400).json({
+                message: "Já existe um usuário cadastrado com esse email!"
+            });
+        }
+        //Criar um novo registro
+        const newUser = userRepository.create(data);
+        //Salvar o registro no banco de dados
+        await userRepository.save(newUser);
+        
         res.status(201).json({
             message: "Usuário cadastrado com  sucesso!",
-            user: data
+            user: newUser,
         });
     }catch(error){
-
+        res.status(500).json({
+            message: "Erro ao cadastrar usuário!",
+        });
     }
     res.send("Cadastrar");
 });
+
 //Exportar a instrução que está dentro da constante router
 export default router;
